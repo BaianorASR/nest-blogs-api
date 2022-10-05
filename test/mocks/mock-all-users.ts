5242188467d9a146270a9107403605bfad2db2fb
BaianorASR <reis.adson2@outlook.com>
2022-10-05 15:39:30 -0300

    feat(users): white_check_mark: test(users)

---
+++ test/mocks/mock-all-users.ts
@@ -0,0 +1,31 @@
+import { User } from '../../src/typeorm/entities/user.entity';
+
+export const MockAllUsers: User[] = [
+  {
+    id: 'uuid-1',
+    displayName: 'John Doe',
+    image: 'https://i.pravatar.cc/150?img=1',
+    email: 'email@email.com',
+    password: '123456',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 'uuid-1',
+    displayName: 'John Doe',
+    image: 'https://i.pravatar.cc/150?img=1',
+    email: 'email@email.com',
+    password: '123456',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 'uuid-1',
+    displayName: 'John Doe',
+    image: 'https://i.pravatar.cc/150?img=1',
+    email: 'email@email.com',
+    password: '123456',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
