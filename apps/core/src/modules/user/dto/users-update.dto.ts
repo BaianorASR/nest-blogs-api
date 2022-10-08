e25d981c4cc62c8b8ca4777f0fb9dd4b34336c32
BaianorASR <reis.adson2@outlook.com>
2022-10-08 01:32:16 -0300

    feat(global): :rocket: init repo

--- apps/core/src/modules/user/dto/users-update.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-
-import { UsersCreateDto } from './users-create.dto';
-
-export class UsersUpdateDto extends PartialType(UsersCreateDto) {}
--- libs/common/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './database/database.module';
-export * from './database/testing-database.module';
